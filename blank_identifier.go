6aae2a1f54acb72687680bf0dfb48a048a00b883
sai-teja-m <31622112+sai-teja-m@users.noreply.github.com>
2018-01-11 22:15:31 +0530

    First Go Hands on Exercise

---
+++ blank_identifier.go
@@ -0,0 +1,15 @@
+//blank identifier
+
+//Blank Identifier allows you to tell the compiler ,you aren't using something.
+
+package main
+
+import "fmt"
+var a int = 10
+
+func main() {
+	//a , _ := 10
+	//c, _:=a
+	b := 20
+	fmt.Println(b)
+}
